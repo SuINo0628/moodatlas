102eb5aa781435019e3ed696742040acf01b90a1
SuINo0628 <1351937938@qq.com>
2026-08-11 22:06:45 +0800

    v46: 右下角加号 FAB 的+换成 Lucide 爱心图标

--- deploy/sw.js
+++ deploy/sw.js
@@ -1,4 +1,4 @@
-const CACHE="mood-atlas-v45-20260811220335";
+const CACHE="mood-atlas-v46-20260811220616";
 const ASSETS=["./","./index.html","./guide.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS).catch(function(){});}).then(function(){return self.skipWaiting();}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
