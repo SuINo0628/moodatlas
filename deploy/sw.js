89d610b7e98cb659ea47ce12c6cea52ce219c683
SuINo0628 <1351937938@qq.com>
2026-08-11 21:38:57 +0800

    v42 记一笔基于聊天整段话：AI 一次分析全部并自动填字段

--- deploy/sw.js
+++ deploy/sw.js
@@ -1,4 +1,4 @@
-const CACHE="mood-atlas-v41-20260811205618";
+const CACHE="mood-atlas-v42-20260811213832";
 const ASSETS=["./","./index.html","./guide.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS).catch(function(){});}).then(function(){return self.skipWaiting();}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
